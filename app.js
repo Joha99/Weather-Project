d9d86d529bd6717141ad9c4f88a157103bc3d29b
Joha Kim <johakim@Johas-MBP.fios-router.home>
2020-06-25 16:42:37 -0400

    get request to openweathermap API and parse data to JSON format

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const express = require("express"); 
+const express = require("express");
+//native Node HTTPS module
+const https = require("https");
 
-// initalize new express app 
-const app = express(); 
+// initalize new express app
+const app = express();
 
-// landing page 
+// landing page
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html"); 
-}); 
+  var url =
+    "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=9e2aa3cdf0b653363eaf05000206ee15&units=metric";
 
-// server running on port 3000, add callback function 
+  // GET request to Openweathermap
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+
+    // get the data from response
+    response.on("data", function (data) {
+      // convert hexadecimal data to JSON format
+      const weatherData = JSON.parse(data);
+      console.log(weatherData); 
+    });
+  });
+});
+
+// server running on port 3000, add callback function
 app.listen(3000, function () {
-    console.log("Server running on port 3000"); 
-}); 
+  console.log("Server running on port 3000");
+});
